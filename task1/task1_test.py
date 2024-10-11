11a13e0ba9bea48e94dc74cd2a8750102c2ee229
kolys <kolysuslo123@gmail.com>
2024-10-12 00:23:04 +0300

    исправил тест к задаче №1

--- task1/task1_test.py
+++ task1/task1_test.py
@@ -4,7 +4,7 @@ from task1 import formula
 
 
 class TestMathFunctions(unittest.TestCase):
-    def test_xyz(self):
+    def test_formula(self):
         self.assertEqual(formula(5, 4), '14253')
         self.assertEqual(formula(12, 10), '11074')
         self.assertEqual(formula(25, 32), '17131925612182451117234101622391521281420')
